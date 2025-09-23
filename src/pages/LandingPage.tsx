fbc6da46f3474321686d2952e2e42b07dd9c57eb
Juan David Calderon Jaramillo <juan.calderon698@pascualbravo.edu.co>
2025-09-23 15:07:38 -0500

    Finish: login
    by Juan Calderon

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,16 +1,46 @@
-import { ReactComponent as GoogleIcon } from '../components/assets/google-icon.svg';
-
-
+import { IconBrandGoogle } from '@tabler/icons-react';
+import { IconMail } from '@tabler/icons-react';
+import { IconLock } from '@tabler/icons-react';
+import { IconBrandApple } from '@tabler/icons-react';
 
 export const LandingPage = () => {
   return (
     <div className="login-container">
-      <h2 className="form-title">Log inwith</h2>
+      <h2 className="form-title">Log in with.</h2>
       <div className="social-login">
         <button className="social-button">
-          <GoogleIcon />
+          <IconBrandGoogle size={30} color="#000" stroke={1.5} />
+          <p>
+            Google
+          </p>
+        </button>
+        <button className="social-button">
+          <IconBrandApple size={30} color="#000" stroke={1.5} />
+          <p>
+            Apple
+          </p>
         </button>
       </div>
+
+      <p className="separator"><span>or</span></p>
+
+      <form action="#" className="login-form">
+        <div className="input-wrapper">
+          <input required type="email" className="input-field" placeholder="Email address" />
+          <IconMail size={30} color="#000" stroke={1.5} />
+        </div>
+      </form>
+
+      <form action="#" className="login-form">
+        <div className="input-wrapper">
+          <input required type="password" className="input-field" placeholder="Password" />
+          <IconLock size={30} color="#000" stroke={1.5} />
+        </div>
+        <a href="#forgot-password" className="forgot-password-link">Forgot password?</a>
+        <button className="login-button">Log in</button>
+      </form>
+
+      <p className="signup-text">Don't have an account? <a href="#signup">Sign up</a></p>
     </div>
-  )
-}
+  );
+};
