c4584db209b70a04d6bbf35871919b0e4a6996a0
Juan David Calderon Jaramillo <juan.calderon698@pascualbravo.edu.co>
2025-09-23 15:10:32 -0500

    Finish log in section

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,46 +1,9 @@
-import { IconBrandGoogle } from '@tabler/icons-react';
-import { IconMail } from '@tabler/icons-react';
-import { IconLock } from '@tabler/icons-react';
-import { IconBrandApple } from '@tabler/icons-react';
 
 export const LandingPage = () => {
   return (
-    <div className="login-container">
-      <h2 className="form-title">Log in with.</h2>
-      <div className="social-login">
-        <button className="social-button">
-          <IconBrandGoogle size={30} color="#000" stroke={1.5} />
-          <p>
-            Google
-          </p>
-        </button>
-        <button className="social-button">
-          <IconBrandApple size={30} color="#000" stroke={1.5} />
-          <p>
-            Apple
-          </p>
-        </button>
-      </div>
-
-      <p className="separator"><span>or</span></p>
-
-      <form action="#" className="login-form">
-        <div className="input-wrapper">
-          <input required type="email" className="input-field" placeholder="Email address" />
-          <IconMail size={30} color="#000" stroke={1.5} />
-        </div>
-      </form>
-
-      <form action="#" className="login-form">
-        <div className="input-wrapper">
-          <input required type="password" className="input-field" placeholder="Password" />
-          <IconLock size={30} color="#000" stroke={1.5} />
-        </div>
-        <a href="#forgot-password" className="forgot-password-link">Forgot password?</a>
-        <button className="login-button">Log in</button>
-      </form>
-
-      <p className="signup-text">Don't have an account? <a href="#signup">Sign up</a></p>
+    <div className="landing-container">
+      <h1>Welcome to the Landing Page</h1>
+      <p>This is the main entry point of the application.</p>
     </div>
   );
 };
