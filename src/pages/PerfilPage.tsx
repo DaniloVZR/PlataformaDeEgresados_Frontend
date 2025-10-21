f9d3876fb07c5722ee7676d1f41b83f749ba6824
MateoRCR <mateorc2006@gmail.com>
2025-10-21 18:09:50 -0500

    Fix: Bug de archivos sin contenido

--- src/pages/PerfilPage.tsx
+++ src/pages/PerfilPage.tsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import PerfilEgresado from "../components/Perfil";
-=======
-import PerfilEgresado from "../components/perfil";
->>>>>>> b301f75000fd29d14043ebdd3efc079b7e4b187e
-
 export default function PerfilPage() {
   return (
     <div className="perfil-page">
